71c78eee69d946a09bc463b090d5827f3cede914
MohamedEmadFahmy <mohamedemadfahmy0@gmail.com>
2023-02-27 02:14:09 +0200

    Create validation code for telephone field Add code to enable iterating over input fields to validate

--- validation.js
+++ validation.js
@@ -1,4 +1,28 @@
 // validation script here
-let reg = /[a-z]{7}/gi;
+// let reg = /[a-z]{7}/gi;
 
-let reg2 = new RegExp(/[a-z]{7}/, "ig");
+// let reg2 = new RegExp(/[a-z]{7}/, "ig");
+
+const inputs = document.querySelectorAll("input");
+
+const patterns = {
+    telephone: /^01([025])(\d{8})$/
+};
+
+//validation function
+function validate(field, regex){
+    if(regex.test(field.value)){
+        field.className = "valid";
+    }
+    else{
+        field.className = "invalid";
+    }
+}
+
+
+inputs.forEach((input) => {
+    input.addEventListener("keyup", (e) => {
+        // console.log(e.target.attributes.name.value );
+        validate(e.target, patterns[e.target.attributes.name.value]);
+    })
+})
